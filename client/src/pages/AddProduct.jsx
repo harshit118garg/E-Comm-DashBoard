a1f2812814a2cf1d500ac97b2bd7c98207f5a373
Harshit Garg <harshit118garg@gmail.com>
2022-10-31 19:17:41 +0530

    add products fields validations

--- client/src/pages/AddProduct.jsx
+++ client/src/pages/AddProduct.jsx
@@ -28,11 +28,24 @@ const AddProduct = () => {
   const [category, setCategory] = useState(types[0]);
   const [brand, setBrand] = useState(deviceBrand[0]);
   const [image, setImgUrl] = useState("");
+  const [error, setError] = useState(false);
 
   const handleProductAdd = async (e) => {
     e.preventDefault();
 
     try {
+      if (
+        !name ||
+        !price ||
+        !image ||
+        brand === deviceBrand[0] ||
+        category === types[0]
+      ) {
+        setError(true);
+        alert("please fill the product details ");
+        return false;
+      }
+
       const userData = JSON.parse(localStorage.getItem("userData"));
       const userId = userData._id;
       let result = await fetch("http://localhost:5000/addproduct", {
@@ -50,6 +63,7 @@ const AddProduct = () => {
       setCategory(types[0]);
       setBrand(deviceBrand[0]);
       setImgUrl("");
+      setError(false);
     } catch (error) {
       console.log(`something viscious happened -> ${error}`);
     }
@@ -65,28 +79,33 @@ const AddProduct = () => {
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
+        {error && !name && <span>Enter valid Name</span>}
         <input
           type="text"
           placeholder="Enter Product Price"
           value={price}
           onChange={(e) => setprice(e.target.value)}
         />
+        {error && !price && <span>Enter valid Price</span>}
         <select onChange={(e) => setCategory(e.target.value)} value={category}>
           {types.map((option, index) => {
             return <option key={index}>{option}</option>;
           })}
         </select>
+        {error && category === types[0] && <span>Select valid Category</span>}
         <select value={brand} onChange={(e) => setBrand(e.target.value)}>
           {deviceBrand.map((deviceBrandName, index) => {
             return <option key={index}>{deviceBrandName}</option>;
           })}
         </select>
+        {error && brand === deviceBrand[0] && <span>Select valid Brand</span>}
         <input
           type="text"
           placeholder="Enter Product Image URL"
           value={image}
           onChange={(e) => setImgUrl(e.target.value)}
         />
+        {error && !image && <span>Enter a proper Image URL</span>}
         <button type="button" className="button" onClick={handleProductAdd}>
           Add Product
         </button>
